f3931c8db89171e450cef0717676731ee2bff721
SapiRoytman <45538694+SapiRoytman@users.noreply.github.com>
2023-02-16 15:59:57 +0200

    test with a different freq

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ from elasticAsync import search
 def test_json_request_body_generator():
     exp_id = "63ea9306a1793526182c2448"
     tag = "voice"
-    start_tfs = 10
-    end_tfs = 50
+    start_tfs = 5
+    end_tfs = 10
     elastic_index = "soos-2"
     time_frame=5
     json_bodies = json_request_body_generator(exp_id, tag, start_tfs, end_tfs, elastic_index)
@@ -13,7 +13,8 @@ def test_json_request_body_generator():
 
 def test_search():
     json_bodies = test_json_request_body_generator()
-    results = search(json_bodies)
+    freq = '793'
+    results = search(json_bodies, freq)
     return results
     
 
